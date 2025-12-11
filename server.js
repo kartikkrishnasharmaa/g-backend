d8cc2e2d459f384e1778ecb1ee1a713ffa7e4994
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2025-12-11 13:55:59 +0530

    Refactor CORS middleware configuration

--- server.js
+++ server.js
@@ -34,7 +34,14 @@ cloudinary.config({
 });
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
 // to send large files
